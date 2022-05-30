1bc930c3da50ba373c21cd179481ccffe440c289
ahmedmalsawi <ahmedmalsawi@gmail.com>
2022-05-30 08:26:41 +0300

    Update connect.php

--- admin/connect.php
+++ admin/connect.php
@@ -1,8 +1,8 @@
 <?php
 
-$dsn = 'mysql:host=localhost;dbname=shop';
-$user = 'root';
-$pass = '';
+$dsn = 'mysql:host=sql6.freemysqlhosting.net;dbname=sql6496300';
+$user = 'sql6496300';
+$pass = 'Tyf1HeNgmD';
 $option = array(
     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
 );
@@ -14,3 +14,4 @@ try {
 } catch (PDOException $e) {
     echo 'Failed to connect ' . $e->getMessage(). '<br>';
 }
+
